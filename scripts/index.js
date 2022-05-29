32a967c7ebf065e85a86d76469a624852941b71b
Alexander Stanford <alexander.stanford94@gmail.com>
2022-05-29 13:35:29 -0700

    update to project 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,21 @@
 const modalwindow = document.querySelector(".popup");
-const editForm = document.querySelector(".pop__form");
+const editForm = document.querySelector(".popup__form");
 const editButton = document.querySelector(".profile__button-edit");
 const closeButton = document.querySelector(".form__exit-button");
 /*const AddButton = document.querySelector(".profile__button-add");*/
 const profileName = document.querySelector(".profile__name");
 const profileJobTitle = document.querySelector(".profile__job-title");
-
-/*const inputValueTitle = formEditProfile.querySelector(".form__input_name");
-const inputValueSubtitle = formEditProfile.querySelector(".form__input_job");*/
-
-/*const nameValue = formCreateElement.querySelector(".form__input_name");*/
-
-/*let popupForm = document.querySelector(".popup");
-let profileName = document.querySelector(".profile__name");
-let profileJobTitle = document.querySelector(".profile__job-title");*/
-const formInputName = document.querySelector(".form__input-name");
-const formInputJob = document.querySelector(".form__input-job");
-let submitBtn = popupForm.querySelector(".form__button");
+const formInputName = editForm.querySelector(".form__input-name");
+const formInputJob = editForm.querySelector(".form__input-job");
+let submitBtn = modalwindow.querySelector(".form__button");
 
 function toggleModalVisibility() {
-  if (!modalwindow.classList.toggle("popup__is-opened"));
-formInputName.value = profileName.textContent;
-formInputJob.value = profileJobTitle.textContent;
-}
+  //if (!modalwindow.classList.contains("popup__is-opened")) {
+  //formInputName.value = profileName.textContent;
+  //formInputJob.value = profileJobTitle.textContent;
+  //}
 
-modalwindow.classList.toggle("popup__is-opened");
+  modalwindow.classList.toggle("popup__is-opened");
 }
 
 function submitForm(event) {
@@ -39,10 +30,3 @@ function submitForm(event) {
 editForm.addEventListener("submit", submitForm);
 editButton.addEventListener("click", toggleModalVisibility);
 closeButton.addEventListener("click", toggleModalVisibility);
-
-
-/*function togglePopupElement(modalwindow) {
-  modalwindow.classList.toggle("popup__is-opened");
-}*/
-
-// Event listeners
