341e9eb6f7b97bb2a2092185c822d87226962dec
Alexander Stanford <alexander.stanford94@gmail.com>
2022-05-31 22:32:23 -0700

    update to project 5

--- scripts/index.js
+++ scripts/index.js
@@ -5,29 +5,52 @@ const closeButton = document.querySelector(".form__exit-button");
 /*const AddButton = document.querySelector(".profile__button-add");*/
 const profileName = document.querySelector(".profile__name");
 const profileJobTitle = document.querySelector(".profile__job-title");
-const formInputName = editForm.querySelector(".form__input-name");
-const formInputJob = editForm.querySelector(".form__input-job");
-let submitBtn = modalwindow.querySelector(".form__button");
+//const formInputName = editForm.querySelector(".form__input-name");
+//const formInputJob = editForm.querySelector(".form__input-job");
+const submitBtn = modalwindow.querySelector(".form__button");
 
-function toggleModalVisibility() {
-  //if (!modalwindow.classList.contains("popup__is-opened")) {
-  //formInputName.value = profileName.textContent;
-  //formInputJob.value = profileJobTitle.textContent;
-  //}
+editButton.addEventListener("click", function () {
+  modalwindow.classList.add("popup__is-opened");
+});
 
-  modalwindow.classList.toggle("popup__is-opened");
-}
+closeButton.addEventListener("click", function () {
+  modalwindow.classList.remove("popup__is-opened");
+});
 
-function submitForm(event) {
+editForm.addEventListener("submit", (event) => {
   event.preventDefault();
+  const formInputName = event.target.name.value;
+  const formInputJob = event.target.job.value;
 
-  profileName.textContent = formInputName.value;
-  profileJobTitle.textContent = formInputJob.value;
+  profileName.textContent = formInputName;
+  profileJobTitle.textContent = formInputJob;
 
-  toggleModalVisibility();
-}
+  modalwindow.classList.remove("popup__is-opened");
+});
+//function toggleModalVisibility() {
+//if (modalwindow.classList.contains("popup__is-opened")) {
+//event.preventDefault();
+//formInputName.value = profileName.textContent;
+//formInputJob.value = profileJobTitle.textContent;
+//}
 
-editForm.addEventListener("submit", submitForm);
-editButton.addEventListener("click", toggleModalVisibility);
-closeButton.addEventListener("click", toggleModalVisibility);
-submitBtn.addEventListener("submit", submitForm);
+//modalwindow.classList.toggle("popup__is-opened");
+//}
+
+//editButton.addEventListener("click", function () {
+//editForm.classList.add("popup__is-opened");
+//});
+
+//function submitForm(event) {
+//event.preventDefault();
+//debugger;
+//profileName.textContent = formInputName.value;
+//profileJobTitle.textContent = formInputJob.value;
+
+//toggleModalVisibility();
+//}
+
+//editForm.addEventListener("submit", submitForm);
+//editButton.addEventListener("click", toggleModalVisibility);
+//closeButton.addEventListener("click", toggleModalVisibility);
+//submitBtn.addEventListener("submit", submitForm);
